a8d2568502ef63ea18afb2882ee3bb66511f7ed3
Mateus Menezes <111348801+Mateusflawer@users.noreply.github.com>
2025-03-02 14:42:24 -0300

    ajuste

--- front-end/src/api/api.js
+++ front-end/src/api/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import "dotenv/config";
 
 const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001" : "/api";
+// const URL = "http://localhost:3001";
+const URL = "https://deploy-spotify-project-uhzq.onrender.com/api";
 
 const artistsArray = await axios
   .get(`${URL}/api/artists`)
