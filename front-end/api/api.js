63e21f30bd0ae8ba48576439745c7c9b832c3c03
Mateus Menezes <111348801+Mateusflawer@users.noreply.github.com>
2025-03-05 20:18:25 -0300

    Correção do endereço da API

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 // const { NODE_ENV } = process.env;
-const URL = "http://localhost:3001/api";
-// const URL = "https://deploy-spotify-project-uhzq.onrender.com/api";
+// const URL = "http://localhost:3001/api";
+const URL = "https://deploy-spotify-project-uhzq.onrender.com/api";
 
 const artistsArray = await axios.get(`${URL}/artists`).then((res) => res.data);
 const songsArray = await axios.get(`${URL}/songs`).then((res) => res.data);
